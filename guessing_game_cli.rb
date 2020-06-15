56828882a7c29cd22469e491add66ffd643a0936
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-06-15 21:46:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,10 @@
   
 
 def run_guessing_game
-  computer_number=rand(5)
+  computer_number=rand(5)+1
   input=gets.chomp
  
-  if input == computer_number+1
+  if input == computer_number
     message = "You guessed the correct number!"
   elsif input =="exit"
     message = "Goodbye!"
